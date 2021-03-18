4cc312eecc2a866bec7477eb6cb5aa44c86ed9aa
arinkhan25 <akhan0119@richmondhillhs.org>
2021-03-18 17:51:24 +0000

    arin school

--- src/App.js
+++ src/App.js
@@ -5,22 +5,35 @@ import Product from './Components/Product.js';
 import Summary from './Components/Summary.js';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-  
-  render() {
-    return (
-      <div className="App">
-        <Hero />
-        <Product type="Laptop" price="999.00" />
-        <Product type="Mechanical Pencil" price="0.25" />
-        <Product type="College Ruled Loose Leaf" price="2.75" />
-        <Summary globalState={this.state} />
-      </div>
-    );
-  }
+constructor(props) {
+super(props);
+this.state = {
+cart: [],
+total: 0,
+
+}
+}
+
+buy = (thing , price) => {
+this.setState = ((state) => {
+state.cart.push(thing)
+state.total += parseFloat(price)
+return state
+})
+}
+
+render() {
+return (
+<div className="App">
+<Hero />
+<Product type="Laptop" price="999.00" buy = {this.buy}/>
+<Product type="Mechanical Pencil" price="0.25" buy = {this.buy} />
+<Product type="College Ruled Loose Leaf" price="2.75" buy = {this.buy} />
+<Product type="Eraser" price="8.00" buy = {this.buy} />
+<Summary globalState={this.state} />
+</div>
+);
+}
 }
 
 export default App;
--- src/Components/Summary.js
+++ src/Components/Summary.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../App.css';
 
-function Summary() {
-  return(
-    <div className="summary">
-      <p>This is a Summary!</p>
-    </div>
-  );
+function Summary(props) {
+return(
+<div className="summary">
+<p>This is a Summary!</p>
+<p>Total: {props.globalState.total}</p>
+<p>{props.globalState.cart}</p>
+</div>
+);
 }
 
 export default Summary;
